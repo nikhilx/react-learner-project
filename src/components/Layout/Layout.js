d607264b07ce5370ecf8637a9ef7a921aa19f9ad
nikhilx <nikhil@memoria-inc.com>
2020-01-10 10:57:40 +0530

    Added sidedrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Toolbar from '../Navigation/Toolbar/Toolbar'
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 import classes from './Layout.module.css'
 
 const Layout = ({ children }) => {
     return (
         <>
             <Toolbar />
+            <SideDrawer />
             <main className={classes.content}>
                 {children}
             </main>
