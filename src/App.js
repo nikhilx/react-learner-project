7a8fa7602f1dbb70d1098f3cc86716a2f2441075
nikhilx <nikhil@memoria-inc.com>
2020-01-26 17:29:37 +0530

    Completed Module

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Layout from './components/Layout/Layout';
+import Layout from './containters/Layout/Layout';
 import BurgerBuilder from './containters/BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
